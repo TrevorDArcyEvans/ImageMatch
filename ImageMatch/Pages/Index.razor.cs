132cbd6ce3d73a0173e02da7947386f548f77dbf
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-04-07 22:58:18 +0100

    - image preview - only enable button if we have two images

--- ImageMatch/Pages/Index.razor.cs
+++ ImageMatch/Pages/Index.razor.cs
@@ -9,15 +9,19 @@ public sealed partial class Index
 {
   private Image<Rgba32> _img1;
   private Image<Rgba32> _img2;
-  private string _text { get; set; }
   private string _img1FileName { get; set; } = "Upload image 1";
-  private string _img2FileName { get; set; } = "Upload image 1";
+  private string _img2FileName { get; set; } = "Upload image 2";
+  private string _img1Url { get; set; } = string.Empty;
+  private string _img2Url { get; set; } = string.Empty;
+  private string _text { get; set; }
+  private bool _canMatch => _img1 is not null && _img2 is not null;
 
   private async Task LoadFile1(InputFileChangeEventArgs e)
   {
     _img1 = await GetImage(e);
     _img1FileName = e.File.Name;
     _text = _img1 is null ? $"<b>{_img1FileName}</b> --> unknown format" : string.Empty;
+    _img1Url = await GetImageString(e);
   }
 
   private async Task LoadFile2(InputFileChangeEventArgs e)
@@ -25,6 +29,7 @@ public sealed partial class Index
     _img2 = await GetImage(e);
     _img2FileName = e.File.Name;
     _text = _img2 is null ? $"<b>{_img2FileName}</b> --> unknown format" : string.Empty;
+    _img2Url = await GetImageString(e);
   }
 
   private static async Task<Image<Rgba32>> GetImage(InputFileChangeEventArgs e)
@@ -42,6 +47,14 @@ public sealed partial class Index
 
     ms.Seek(0, SeekOrigin.Begin);
     return Image.Load<Rgba32>(ms);
+  }
+
+  private static async Task<string> GetImageString(InputFileChangeEventArgs e)
+  {
+    var imgFile = e.File;
+    var buffers = new byte[imgFile.Size];
+    await imgFile.OpenReadStream().ReadAsync(buffers);
+    return $"data:{imgFile.ContentType};base64,{Convert.ToBase64String(buffers)}";    
   }
 
   private async Task OnMatch()
