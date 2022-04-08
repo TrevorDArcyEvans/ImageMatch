efc78a824235b0eafaebd43f265776323de0944c
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2022-04-08 09:29:52 +0100

    - skeleton CLI client

---
+++ ImageMatch.UI.CLI/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ImageMatch.UI.CLI;
+
+using CommandLine;
+
+internal static class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var result = await Parser.Default.ParseArguments<Options>(args)
+      .WithParsedAsync(Run);
+    await result.WithNotParsedAsync(HandleParseError);
+  }
+
+  private static async Task Run(Options opt)
+  {
+  }
+
+  private static Task HandleParseError(IEnumerable<Error> errs)
+  {
+    if (errs.IsVersion())
+    {
+      Console.WriteLine("Version Request");
+      return Task.CompletedTask;
+    }
+
+    if (errs.IsHelp())
+    {
+      Console.WriteLine("Help Request");
+      return Task.CompletedTask;
+      ;
+    }
+
+    Console.WriteLine("Parser Fail");
+    return Task.CompletedTask;
+  }
+}
