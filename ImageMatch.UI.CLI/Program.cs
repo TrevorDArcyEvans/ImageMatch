0c97302939c3dc272fc7b31fa81469600d2398d3
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2022-04-08 09:52:36 +0100

    - implemented command line compare

--- ImageMatch.UI.CLI/Program.cs
+++ ImageMatch.UI.CLI/Program.cs
@@ -1,6 +1,9 @@
 ﻿namespace ImageMatch.UI.CLI;
 
 using CommandLine;
+using ImagesComparator;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 internal static class Program
 {
@@ -13,6 +16,14 @@ internal static class Program
 
   private static async Task Run(Options opt)
   {
+    var img1 = await Image.LoadAsync<Rgba32>(opt.ImageFile1Path);
+    var img2 = await Image.LoadAsync<Rgba32>(opt.ImageFile2Path);
+    var imgHash1 = new ImgHash(img1);
+    var imgHash2 = new ImgHash(img2);
+    var match = imgHash1.CompareWith(imgHash2);
+    Console.WriteLine($"ImageFile1 = {opt.ImageFile1Path}");
+    Console.WriteLine($"ImageFile2 = {opt.ImageFile2Path}");
+    Console.WriteLine($"  Match = {match}%");
   }
 
   private static Task HandleParseError(IEnumerable<Error> errs)
